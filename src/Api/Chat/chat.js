83b6e6a0328cb5b2bbdf5353850201196d14d6fb
anwar789456 <83927082+anwar789456@users.noreply.github.com>
2025-02-17 09:53:46 +0100

    Update chat.js

--- src/Api/Chat/chat.js
+++ src/Api/Chat/chat.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL_GET = 'http://localhost:3001/api/get-chat-from-database';
-
-// const API_BASE_URL_GET = 'https://www.samethome.com/api/get-chat-from-database';
+const API_BASE_URL_GET = 'https://www.samethome.com/api/get-chat-from-database';
 /**
  * Fetch all chat items.
  */
